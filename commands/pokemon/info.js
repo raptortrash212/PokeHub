7e8bdb981f7a0f0cb18f111ebf00ac690269de82
Striker <zzstrikerzzyt@gmail.com>
2019-09-11 22:27:44 +0100

    FIXXXXXX

--- commands/pokemon/info.js
+++ commands/pokemon/info.js
@@ -50,12 +50,13 @@ class InfoCommand extends Command {
 
         // Female or Male Pokemon (needs to be seperate)
         if (pokemonName[pokemonName.length - 1] == 'f' || pokemonName[pokemonName.length - 1] == 'm') {
-            pokemonName = pokemonName.slice(0, pokemonName.length - 1);
-            if (pokemonName[pokemonName.length] == 'm') {
+            if (pokemonName[pokemonName.length - 1] == 'm') {
+                pokemonName = pokemonName.slice(0, pokemonName.length - 1);
                 r = await get(`https://pokeapi.co/api/v2/pokemon/${pokemonNameLower}-m`);
                 const pokeObj = r.body;
                 PID = pokeObj.id;
             } else {
+                pokemonName = pokemonName.slice(0, pokemonName.length - 1);
                 r = await get(`https://pokeapi.co/api/v2/pokemon/${pokemonNameLower}-f`);
                 const pokeObj = r.body;
                 PID = pokeObj.id;
