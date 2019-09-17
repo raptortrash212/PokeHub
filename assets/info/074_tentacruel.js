48233b45074a1aa41bda17739f6b4b30436bbc07
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-09-16 18:44:05 -0700

    Create 074_tentacruel.js

---
+++ assets/info/074_tentacruel.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 73, 
+    'species': '**Tentacruel** (**ドククラゲ** *Dokukurage*)', 
+    'types' : 'Water | Poison',
+    'pokedexEntry': 'It fires off ultrasonic waves from its red orbs to weaken its prey, and then it wraps them up in its 80 tentacles. It communicates with others of its kind by lighting up the red orbs on its head. When the orbs are blinking, it\'s a warning sign.\n\n*Dex entry from USUM*', 
+    'genderRatio': { M: 0.5, F: 0.5 }, 
+    'catchRate': { integer: 60, percentage: '14.8%' }, 
+    'baseStats': { hp: 80, atk: 70, def: 65, spAtk: 80, spDef: 120, spd: 100 }, 
+    'abilities': { 0: 'Clear Body *or* Liquid Ooze', 'H': 'Rain Dish' },
+    'height': 1.6, 
+    'weight': 121.3, 
+    'colorHex': '#9DD2EE', 
+    'colorHexShiny': '#C8B9E5',
+    'color': 'Very Soft Blue', 
+    'colorShiny': 'Light Grayish Violet',
+    'evolvesFrom': { species: 'Tentacool', level: 30, triggeredBy: 'Levelling Up' },
+    'eggGroups': ['Water 3'],
+    'hatchTime': '5140 - 5396 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/tentacruel.gif', 
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/tentacruel.gif',
+    'legendary': false, 
+    'mythical': false
+};
