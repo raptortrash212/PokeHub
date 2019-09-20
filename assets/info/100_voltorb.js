4f99ae7175c9bc2eba32d75274417a201f7f60d9
Striker <34070194+Strikerrr@users.noreply.github.com>
2019-09-20 15:48:15 +0100

    Update 100_voltorb.js

--- assets/info/100_voltorb.js
+++ assets/info/100_voltorb.js
@@ -3,7 +3,7 @@ exports.info = {
     'species': '**Voltorb** (**ビリリダマ** *Biriridama*)', 
     'types' : 'Electric',
     'pokedexEntry': 'Voltorb was first sighted at a company that manufactures Poké Balls. The link between that sighting and the fact that this Pokémon looks very similar to a Poké Ball remains a mystery. Voltorb is extremely sensitive - it explodes at the slightest of shocks. It is rumored that it was first created when a Poké Ball was exposed to a powerful pulse of energy.\n\n*Dex entry from ORAS*', 
-    'genderRatio': { m: 0, f: 0 }, 
+    'genderRatio': { M: 0, F: 0 }, 
     'catchRate': { integer: 190, percentage: '35.2%' }, 
     'baseStats': { hp: 40, atk: 30, def: 50, spAtk: 5, spDef: 5, spd: 100 }, 
     'abilities': { 0: 'Soundproof *or* Static', 'H': 'Aftermath' },
