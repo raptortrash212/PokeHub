c0fac2693ef56f56465d1e6b42760863c6e9b964
Striker <zzstrikerzzyt@gmail.com>
2019-09-21 20:19:09 +0100

    Potential fix

--- src/pokemonFetcher.js
+++ src/pokemonFetcher.js
@@ -8,7 +8,7 @@ class pokemonFetcher {
         if (this.args[0].toLowerCase() == 'shiny' && this.args[1].toLowerCase() == pokemonNameLower && !this.args[2] && !this.args[3]) {
             console.log('Regular Shiny Poke');
             let pokemonObject;
-            return pokemonObject = require(`../../assets/info/${PID}_${pokemonNameLower}.js`).info;
+            return pokemonObject = require(`../assets/info/${PID}_${pokemonNameLower}.js`).info;
         }
     }
 
@@ -17,7 +17,7 @@ class pokemonFetcher {
         if (this.args[0].toLowerCase() != 'mega' && this.args[0].toLowerCase() != 'shiny' && !this.args[1] && !this.args[2] && !this.args[3]) {
             console.log('Regular Poke');
             let pokemonObject;
-            return pokemonObject = require(`../../assets/info/${PID}_${pokemonNameLower}.js`).info;
+            return pokemonObject = require(`../assets/info/${PID}_${pokemonNameLower}.js`).info;
         }
     }
 
