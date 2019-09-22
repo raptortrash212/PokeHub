95b810b66bd1b0a6f40b664083922ec783662f69
Striker <zzstrikerzzyt@gmail.com>
2019-09-22 20:15:23 +0100

    Add Alolan Ninetales (38)

---
+++ assets/info/Gen 1/038_alolan-ninetales.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 38,
+    'species': '**Alolan Nintetales** (**アローラキュウコン** *Alola Kyuukon*)',
+    'types': 'Ice | Fairy',
+    'pokedexEntry': 'The reason it guides people all the way down to the mountain\'s base is that it wants them to hurry up and leave. Some said that deities visited mountains where Ninetales live, so in times long past, no one was allowed to go into those mountains.\n\n*Dex entry from USUM*',
+    'genderRatio': { M: 0.75, F: 0.25 },
+    'catchRate': { integer: 75, percentage: '17.5%' },
+    'baseStats': { hp: 73, atk: 67, def: 75, spAtk: 81, spDef: 100, spd: 109 },
+    'abilities': { 0: 'Snow Cloak', 'H': 'Snow Warning' },
+    'height': 1.1,
+    'weight': 43.9,
+    'colorHex': '#F3FFFF',
+    'colorHexShiny': '#EFEDFF',
+    'color': 'Very Pale (Mostly White) Cyan',
+    'colorShiny': 'Very Pale (Mostly White) Blue',
+    'evolvesFrom': { species: 'Alolan Vulpix', level: null, triggeredBy: 'Item Use (Ice Stone)' },
+    'eggGroups': ['Field'],
+    'hatchTime': '5140 - 5396 steps',
+    'gif': 'http://play.pokemonshowdown.com/sprites/xyani/ninetales-alola.gif',
+    'gifShiny': 'http://play.pokemonshowdown.com/sprites/xyani-shiny/ninetales-alola.gif',
+    'legendary': false,
+    'mythical': false
+};
