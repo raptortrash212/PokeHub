fe79b826f61d7f5fa1b66da57024fbe7104acbf7
Striker <zzstrikerzzyt@gmail.com>
2019-09-22 20:05:46 +0100

    Add Ninetales (38)

---
+++ assets/info/Gen 1/038_ninetales.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 38,
+    'species': '**Nintetales** (**キュウコン** *Kyuukon*)',
+    'types': 'Fire',
+    'pokedexEntry': 'It is vindictive and relentless by nature. Those who cross it even once will be cursed for a thousand years, along with their descendants. The flickering flames it spews from its mouth leave its opponents hypnotized. Then, this extremely intelligent Pokémon attacks.\n\n*Dex entry from USUM*',
+    'genderRatio': { M: 0.75, F: 0.25 },
+    'catchRate': { integer: 75, percentage: '17.5%' },
+    'baseStats': { hp: 73, atk: 76, def: 75, spAtk: 81, spDef: 100, spd: 100 },
+    'abilities': { 0: 'Flash Fire', 'H': 'Drought' },
+    'height': 1.1,
+    'weight': 43.9,
+    'colorHex': '#FFFF94',
+    'colorHexShiny': '#DBD7E2',
+    'color': 'Very Light Yellow',
+    'colorShiny': 'Light Grayish Violet',
+    'evolvesFrom': { species: 'Vulpix', level: null, triggeredBy: 'Item Use (Fire Stone)' },
+    'eggGroups': ['Field'],
+    'hatchTime': '5140 - 5396 steps',
+    'gif': 'http://play.pokemonshowdown.com/sprites/xyani/ninetales.gif',
+    'gifShiny': 'http://play.pokemonshowdown.com/sprites/xyani-shiny/ninetales.gif',
+    'legendary': false,
+    'mythical': false
+};
