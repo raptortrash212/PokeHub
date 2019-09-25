47e4cf18497ac0809d57e73ab037544f43581799
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-09-24 17:48:33 -0700

    Create 749_mudbray.js

---
+++ assets/info/Gen 7/749_mudbray.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 749, 
+    'species': '**Mudbray** (**ドロバンコ** *Dorobanko*)', 
+    'types' : 'Ground',
+    'pokedexEntry': 'It loves playing in the mud. If it isn't showered with mud on a daily basis, it gets stressed out and stops listening to its Trainer. Possessing mighty power, Mudbray can be loaded with 50 times its own body weight and will be totally fine.\n\n*Dex entry from USUM*', 
+    'genderRatio': { M: 0.5, F: 0.5 }, 
+    'catchRate': { integer: 10, percentage: '28.0%' }, // Can find on Bulbapedia
+    'baseStats': { hp: 70, atk: 100, def: 70, spAtk: 45, spDef: 55, spd: 45 }, 
+    'abilities': { 0: 'Own Tempo *or* Stamina	', 'H': 'Inner Focus' },
+    'height': 1.0, 
+    'weight': 242.5, 
+    'colorHex': '#B3826C', 
+    'colorHexShiny': '#D59A2E',
+    'color': 'Slightly Desaturated Orange', 
+    'colorShiny': 'Strong Orange',
+    'evolvesTo': { species: 'Mudsdale', level: '30', triggeredBy: 'Levelling Up' }, // If you need help with this, ask. I can correct you also if you're unsure.
+    'eggGroups': ['Field'],
+    'hatchTime': '5140 - 5396 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/mudbray.gif', 
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/mudbray.gif',
+    'legendary': false, 
+    'mythical': false
+};
