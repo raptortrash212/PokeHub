08f326da6d03733eea05f82dc1cd2c88ef56d02c
raptortrash212 <54644526+raptortrash212@users.noreply.github.com>
2019-09-28 12:21:05 -0700

    Added Persian (053)
    Meoooooow thats right

---
+++ assets/info/Gen 1/053_persian.js
@@ -0,0 +1,23 @@
+exports.info = {
+    'number': 53, 
+    'species': '**Persian** (**ペルシアン** *Persian*)', 
+    'types' : 'Normal',
+    'pokedexEntry': 'Although the jewel on its forehead appears to be a different color than those of Alolan Persian, it\'s mostly made of the same material. This Pokémon is popular with rich people. It's also targeted by hunters who are after the jewel in its forehead.\n\n*Dex entry from USUM*', 
+    'genderRatio': { M: 0.5, F: 0.5 }, 
+    'catchRate': { integer: 90, percentage: '20.1%' }, 
+    'baseStats': { hp: 65, atk: 70, def: 60, spAtk: 65, spDef: 65, spd: 115 }, 
+    'abilities': { 0: 'Limber *or* Technician', 'H': 'Unnerve' },
+    'height': 1.0, 
+    'weight': 70.5, 
+    'colorHex': '#FDEBC0', 
+    'colorHexShiny': '#FAF6C8',
+    'color': 'Very Soft Orange', 
+    'colorShiny': 'Very Soft Yellow',
+    'evolvesTo': { species: 'Meowth', level: '28', triggeredBy: 'Levelling Up' }, 
+    'eggGroups': ['Field'],
+    'hatchTime': '5140 - 5396 steps',
+    'gif': 'https://play.pokemonshowdown.com/sprites/xyani/persian.gif', 
+    'gifShiny': 'https://play.pokemonshowdown.com/sprites/xyani-shiny/persian.gif',
+    'legendary': false, 
+    'mythical': false
+};
